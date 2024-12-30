88133ea828fc1c83b92e1019b920ed30aedd187c
ispobock <ispobaoke@163.com>
2024-12-30 17:36:33 +0000

    add example

--- sgl-kernel/setup.py
+++ sgl-kernel/setup.py
@@ -85,8 +85,10 @@ ext_modules = [
             "src/sgl-kernel/csrc/trt_reduce_internal.cu",
             "src/sgl-kernel/csrc/trt_reduce_kernel.cu",
             "src/sgl-kernel/csrc/moe_align_kernel.cu",
+            "src/sgl-kernel/csrc/test_cutlass_kernel.cu",
             "src/sgl-kernel/csrc/sgl_kernel_ops.cu",
         ],
+        include_dirs=include_dirs,
         extra_compile_args={
             "nvcc": nvcc_flags,
             "cxx": cxx_flags,
